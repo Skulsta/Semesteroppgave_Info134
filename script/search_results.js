97fa86ecedbed776632cbad57394371a0257065a
Stian Skulstad <sti.sku@gmail.com>
2017-03-27 11:59:06 +0200

    Masse rart i et forsøk på å få på plass søkefunksjonen.

---
+++ script/search_results.js
@@ -0,0 +1,60 @@
+/* Her kan dere implementere en søkefunksjon. For eksempel:
+function search_for_X() {
+}
+*/
+function searchForMovie(t) {
+	e.preventDefault();
+	var results = movies_object.filter(a => a.otitle === input.value);
+
+
+
+/* Her kan dere implementere en display function som viser resulatetene av søket. For eksempel:
+function display_X() {
+}
+*/
+function displayResults(results) {
+	var html = results.map(animal => `<li>${animal.type} har en ${animal.color.toLowerCase()} farge</li>`).join("");
+	resultList.innerHTML = html;
+}
+
+function resetSearchResults() {
+	resultList.innerHTML = "";
+}
+
+
+
+window.onload = function() {
+	query_params = get_query_string_parameters();
+
+	search_results = movies_object;
+	
+	if (query_params.film_title) {
+        film_title = document.getElementById("film_title");
+		//Her kan dere for eksempel kalle en søkefunksjon som søker for tittel.
+		searchForMovie();
+    }
+	
+	if (query_params.actor) {
+        actor = document.getElementById("actor");
+		actor.innerHTML = query_params.actor;
+    }
+	
+	if (query_params.director) {
+		director = document.getElementById("director");
+		director.innerHTML = query_params.director;
+    }
+	
+	if (query_params.genre) {
+        genre = document.getElementById("genre");
+		genre.innerHTML = query_params.genre;
+    }
+	
+	if (query_params.country) {
+        country = document.getElementById("country");
+		country.innerHTML = query_params.country;
+    }
+	
+	//Her kan dere for eksempel kalle en (display) funksjon som viser søkeresultater 
+
+	displayResults();
+}
