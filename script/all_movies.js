c31499b0e9601b7cf7378029dfcc6b201c1fdbb2
VegarE <vegar.ellingsen@gmail.com>
2017-03-30 20:42:03 +0200

    all_movies

--- script/all_movies.js
+++ script/all_movies.js
@@ -1,22 +1,27 @@
 window.onload = function() {
     list_element = document.getElementById("all_movies");
     var movies = [];
+    var i = 0;
 
+    
     for (movie_id in movies_object){
-        movie_details = movies_object[movie_id];
-        list_item = document.createElement("LI");
+        for(i; i < 10; i++){
+            movie_details = movies_object[movie_id];
+            list_item = document.createElement("LI");
 
-	// prepare the link
-        item_link = document.createElement("A");
-        item_link.href = "show_movie.html?id=" + movie_id;
-		link_text = document.createTextNode(movie_details["otitle"]);
-        item_link.appendChild(link_text);
-	
-        list_item.appendChild(item_link);
-        list_element.appendChild(list_item); 
-    }
+    	// prepare the link
+            item_link = document.createElement("A");
+            item_link.href = "show_movie.html?id=" + movie_id;
+    		link_text = document.createTextNode(movie_details["otitle"]);
+            item_link.appendChild(link_text);
 
-    for (var i = 0; i < 5; i++){
-        movies.push(list_element[i]);
+            list_item.appendChild(item_link);
+            list_element.appendChild(list_item);  
+            movies.push(list_item);  
+        }
     }
+    console.log(movies); 
 }
+
+
+
