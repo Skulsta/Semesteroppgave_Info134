b6c351f3ee967b5de19c541584573426aef9ed87
Stian Skulstad <sti.sku@gmail.com>
2017-04-16 19:01:47 +0200

    So, many, changes.

--- script/all_movies.js
+++ script/all_movies.js
@@ -1,7 +1,7 @@
 /** Javascript that will create basically the whole front page by
 creating new elements and adding information from the movies. */
 
-var movie_id = 830; // Global variable so that the different categories don't just repeat the same movies.
+var movie_id = 2345; // Global variable so that the different categories don't just repeat the same movies.
 
 
 window.onload = function() {
@@ -20,8 +20,6 @@ function addMoviePicture() {
     while (i < 14) {
             if (movies_object[movie_id] != null) {
             movie_details = movies_object[movie_id];
-
-            rating = movie_details["rating"];
 
             // prepare the link
             dropDown = document.createElement("div");
@@ -54,16 +52,112 @@ function addMoviePicture() {
             forsideBilde.src = src;
 
 
+            dropDownInnholdFilm = document.createElement("div");
+            dropDownInnholdFilm.className = "dropDownInnholdFilm";
+
+
+
+
+
+            /**
+            details = reviews_object[movie_id];
+
+            if (details) {
+            console.log(details);
+        }
+
+            
+            for (object in reviews_object) {
+                if (object == movie_id) {
+                    review_details = reviews_object[object];
+                    rating = document.createTextNode(review_details["rating"] + "/6");
+
+                    dropDownInnholdFilm.appendChild(rating);
+                }
+            }
+            
+
+
+
+            for (var object in reviews_object) {
+                if (object == movie_id)
+                console.log(object);
+                }
+
+                */
+                var rating = 0;
+
+                /** Retrieves rating from reviews.js. */
+                for (var key in reviews_object) {
+                    if (key == movie_id) {
+                    if (!reviews_object.hasOwnProperty(key)) continue;
+
+                    var object = reviews_object[key];
+                    for (var property in object) {
+                        if (!object.hasOwnProperty(property)) continue;
+
+                        userinfo = object[property];
+
+                        if (userinfo["rating"]) {
+                            rating = (userinfo["rating"]);
+                        } 
+                    }
+                }
+            }
+
+
+            /** Get the star icon next to the rating. */
+            star = document.createElement("img");
+            star.className = "star";
+            var starSrc = 'star-icon.png';
+            star.src = starSrc;
+
+            ratingItem = document.createTextNode(rating + "/6");
+
+
+            /** Movie title */
+            var movieTitle = document.createElement("p");
+            var boldTitle = document.createElement("b");
+            boldTitle.textContent = movie_details["otitle"];
+
+            /** The movie description. Set a limit of 140 characters. */
+            var description = document.createElement("p");
+            content = movie_details["description"];
+            
+            if (content == null)
+                content = " [ Ingen beskrivelse ] ";
+
+            if (content.length > 140)
+                content = content.substring(0,140) + "...";
+            description.textContent = content;
+
+
+            /** A lot of appending children */
+
+            movieTitle.appendChild(boldTitle);
+
+            dropDownInnholdFilm.appendChild(ratingItem);
+
+            dropDownInnholdFilm.appendChild(star);
+
+            dropDownInnholdFilm.appendChild(movieTitle);
+
+            dropDownInnholdFilm.appendChild(description);
+
+
             item_link.appendChild(forsideBilde);
 
             dropDownKnapp.appendChild(item_link);
 
+
             dropDown.appendChild(dropDownKnapp);
+
+            dropDown.appendChild(dropDownInnholdFilm);
+
 
             main_bilder.appendChild(dropDown);
 
             left[0].appendChild(main_bilder);
-
 
 
             movie_id++;
